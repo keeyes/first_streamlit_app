c3f239ae605666438912d57d6e887b1be9a4f8db
keeyes <72693591+keeyes@users.noreply.github.com>
2023-03-27 16:58:35 +0800

    Update streamlit_app.py
    edit api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 #api call (no need key) for fruityvice
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
