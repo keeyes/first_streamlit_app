4922d7a63acfae104dbf24134c215dd9b9ec29ea
keeyes <72693591+keeyes@users.noreply.github.com>
2023-03-27 18:53:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #New seciton to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+def get_fruityvice_data(this_fruit_choice):
+    #api call (no need key) for fruityvice
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #take the json format and normalize it
+    fruityvice_normalized =  pandas.json_normalize(fruityvice_response.json())  
+    return fruitvice_normalized
+  
+  
+streamlit.header("Fruityvice Fruit Advice!"
+
 try:
   #user input
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    streamlit.write('The user entered ', fruit_choice)
-    #api call (no need key) for fruityvice
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #take the json format and normalize it
-    fruityvice_normalized =  pandas.json_normalize(fruityvice_response.json())
+    from_function = get_fruitvice_data(fruit_choice)
+    streamlit.write('The user entered ', from_function)
     #output as table
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(from_function)
 except URLError as e:
   streamlit.error()
 #don't run anything beyond here for streamlit
