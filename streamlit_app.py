38806d24aecdcc96a8181da2371ae399963f9c3e
keeyes <72693591+keeyes@users.noreply.github.com>
2023-03-27 17:00:06 +0800

    Update streamlit_app.py
    present json data from fruity vice api as a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 #api call (no need key) for fruityvice
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json()) #json formatted data
+
+#take the json format and normalize it
+fruityvice_normalized =  pandas.json_normalize(fruityvice_response.json())
+#output as table
+streamlit.dataframe(fruityvice_normalized)
