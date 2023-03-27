e84a9e2351f5cb4f895b91169e37efc98bbcb91a
keeyes <72693591+keeyes@users.noreply.github.com>
2023-03-27 18:48:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,19 +25,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New seciton to display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-#uyser input
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#api call (no need key) for fruityvice
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json format and normalize it
-fruityvice_normalized =  pandas.json_normalize(fruityvice_response.json())
-#output as table
-streamlit.dataframe(fruityvice_normalized)
-
+try:
+  #user input
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information."
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    #api call (no need key) for fruityvice
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #take the json format and normalize it
+    fruityvice_normalized =  pandas.json_normalize(fruityvice_response.json())
+    #output as table
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 #don't run anything beyond here for streamlit
 streamlit.stop()
 #snowflake data
